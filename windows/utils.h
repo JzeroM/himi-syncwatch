1c722b550c496fe07167bdb6028140a4ba4f4746
JzeroM <18895392506@163.com>
2026-09-07 09:17:46 +0800

    fix: 使用 Flutter SDK 模板重建平台文件 - Android: 使用 android-kotlin 模板 + 权限 - iOS: 使用 ios 模板 + 麦克风/后台模式 - Windows: 使用 windows 模板 - macOS: 使用 macos 模板 + 网络/音频权限 - Linux: 使用 linux 模板

--- windows/utils.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <vector>
-
-std::vector<std::string> ConvertToCommandLineArguments(int argc, char* argv[]);
-
-std::wstring Utf16FromUtf8(const std::string& utf8_string);
