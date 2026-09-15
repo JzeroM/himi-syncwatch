1af04b4a86db60773dcf04de23d96b459fb5522c
JzeroM <18895392506@163.com>
2026-09-15 11:54:39 +0800

    fix: 恢复 media_kit/ffi/ffi.dart 导入（calloc/toNativeUtf8 来源）

--- lib/services/decode_mode_service.dart
+++ lib/services/decode_mode_service.dart
@@ -2,6 +2,8 @@ import 'dart:ffi';
 import 'dart:collection';
 import 'dart:io';
 // ignore: implementation_imports
+import 'package:media_kit/ffi/ffi.dart';
+// ignore: implementation_imports
 import 'package:media_kit/src/player/native/core/native_library.dart';
 // ignore: implementation_imports
 import 'package:media_kit/generated/libmpv/bindings.dart' as generated;
